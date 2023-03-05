465ee4f6874ad0bc33d7d1b2522bf8668fd1c3dd
Andrey <bakarbyikov@gmail.com>
2023-03-05 17:20:03 +0500

    Added type hints

--- misc.py
+++ misc.py
@@ -1,12 +1,15 @@
 from functools import wraps
 from time import perf_counter
-from typing import Any, Callable
+from typing import Any, Callable, ParamSpec, TypeVar
 
 from settings import *
 
-def print_elapsed_time(func: Callable) -> Callable:
+P = ParamSpec("P")
+R = TypeVar("R")
+
+def print_elapsed_time(func: Callable[P, R]) -> Callable[P, R]:
     @wraps(func)
-    def inner(*args, **kwargs) -> Any:
+    def inner(*args: P.args, **kwargs: P.kwargs) -> R:
         then = perf_counter()
         res = func(*args, **kwargs)
         elapsed = perf_counter() - then
