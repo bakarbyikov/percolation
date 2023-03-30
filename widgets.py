6c5427ed056392d226fd2628d6b3fb6ec4fdca79
Andrey <bakarbyikov@gmail.com>
2023-03-30 20:34:47 +0500

    Refactored Property_scale to separeted classes

--- widgets.py
+++ widgets.py
@@ -7,64 +7,85 @@ from settings import *
 
 class Property_scale(tk.Frame):
 
-    def __init__(self, parent, name: str, callback: Callable, *,
+    def __init__(self, parent, name: str, command: Callable, *,
                  from_: int|float=0, to: int|float=1, 
                  value: int|float=0, step: int|float=1):
         super().__init__(parent)
-        self.callback = callback
+        self.callback = command
         self.value = value
-        self.out_float = not (isinstance(step, int) or step.is_integer())
-        if self.out_float:
-            self.n_digits = min(len(str(step).split('.')[1]), 6)
-            self.mul = 10**self.n_digits
-        else:
-            self.n_digits = 0
-            self.mul = 1
-
+        
         top_part = ttk.Frame(self)
         top_part.pack(fill='x', pady=(5, 0), padx=5)
-
         ttk.Label(top_part, text=name+":").pack(side=tk.LEFT)
-        vcmd = (self.register(self.validate))
-        self.entry = ttk.Spinbox(top_part, 
-                                 from_=from_, to=to,
-                                 increment=step,
-                                 command=self.do_callback,
-                                 validate='all',
-                                 validatecommand=(vcmd, '%P'))
-        self.entry.bind('<Return>', self.do_callback)
-        self.entry.bind('<FocusOut>', self.do_callback)
-        self.update_label(value)
+        self.entry = Spinbox(top_part,
+                             from_=from_, to=to,
+                             step=step, value=value,
+                             command=self.on_entry_update)
         self.entry.pack(side=tk.RIGHT)
 
         bottom_part = ttk.Frame(self)
         bottom_part.pack(pady=(0, 15))
-    
-        self.scale = ttk.Scale(bottom_part, from_=from_*self.mul, to=to*self.mul, 
-                               orient=tk.HORIZONTAL, length=SCALE_LENGHT, 
-                               command=self.update_label)
-        self.scale.set(value*self.mul)
+        self.scale = Scale(bottom_part, 
+                           from_=from_, to=to,
+                           step=step, value=value,
+                           orient=tk.HORIZONTAL, length=SCALE_LENGHT, 
+                           command=self.on_scale_update)
         self.scale.bind("<ButtonRelease-1>", self.do_callback)
         self.scale.pack()
     
-    def update_label(self, new_value: str) -> None:
-        new_value = int(float(new_value))
-        if self.out_float:
-            new_value /= self.mul
-        self.entry.delete(0, tk.END)
-        self.entry.insert(0, round(new_value, self.n_digits))
-        pass
+    def on_entry_update(self, value: int|float) -> None:
+        self.scale.set(value)
+        self.do_callback()
+    
+    def on_scale_update(self, value: int|float) -> None:
+        self.entry.set(value)
     
     def do_callback(self, *_) -> None:
-        if self.out_float:
-            value = float(self.entry.get())
-        else:
-            value = round(float(self.entry.get()))
-        self.scale.set(float(self.entry.get())*self.mul)
+        value = self.entry.get()
         if self.value == value:
             return
         self.value = value
         self.callback(value)
+    
+
+class Spinbox(ttk.Spinbox):
+    
+    def __init__(self, parent, *, from_: int|float=0, to: int|float=1,
+                 step: int|float=1, value: int|float=0,
+                 command: Callable[[int|float], None], **kwargs) -> None:
+        self.callback = command
+        self.ignore_callback = False
+        self.out_float = not (isinstance(step, int) or step.is_integer())
+
+        super().__init__(parent, from_=from_, to=to,
+                         command=self.command, increment=step, **kwargs)
+        
+        vcmd = self.register(self.validate)
+        self.configure(validate='all', validatecommand=(vcmd, '%P'))
+        
+        self.bind('<Return>', self.command)
+        self.bind('<FocusOut>', self.command)
+        self.set(value)
+        
+    def _process_value(self, value: str) -> int|float:
+        if self.out_float:
+            return float(value)
+        return round(float(value))
+    
+    def command(self, *_) -> None:
+        if self.ignore_callback:
+            return
+        value = self.get()
+        self.callback(value)
+    
+    def set(self, value: int|float) -> None:
+        self.ignore_callback = True
+        self.delete(0, tk.END)
+        self.insert(0, value)
+        self.ignore_callback = False
+
+    def get(self) -> int|float:
+        return self._process_value(super().get())
     
     def validate(self, P: str) -> bool:
         if len(P) == 0:
@@ -75,3 +96,55 @@ class Property_scale(tk.Frame):
             return False
         else:
             return True
+
+
+class Scale(ttk.Scale):
+
+    def __init__(self, parent, *, from_: int|float=0, to: int|float=1,
+                 step: int|float=1, value: int|float=0,
+                 command: Callable[[int|float], None], **kwargs) -> None:
+        self.callback = command
+        self.ignore_callback = False
+        self.out_float = not (isinstance(step, int) or step.is_integer())
+        if self.out_float:
+            self.n_digits = min(len(str(step).split('.')[1]), 6)
+            self.mul = 10**self.n_digits
+            from_ *= self.mul
+            to *= self.mul
+        else:
+            self.n_digits = 0
+            self.mul = 1
+
+        super().__init__(parent, from_=from_, to=to, command=self.command, **kwargs)
+        self.set(value)
+    
+    def _process_value(self, value: float) -> int|float:
+        value = round(value)
+        if self.out_float:
+            value /= self.mul
+        return value
+    
+    def command(self, value: str) -> None:
+        if self.ignore_callback:
+            return
+        value = self._process_value(float(value))
+        self.callback(value)
+    
+    def set(self, value: int|float) -> None:
+        self.ignore_callback = True
+        super().set(value*self.mul)
+        self.ignore_callback = False
+    
+    def get(self) -> int|float:
+        return self._process_value(super().get())
+        
+
+if __name__ == "__main__":
+    from random import random
+    root = tk.Tk()
+    entry = Property_scale(root, "hello", step=1, to=100, command=lambda x: print(f"Now value: {x}"))
+    entry.pack(padx=40, pady=(40, 5))
+    b = ttk.Button(root)
+    b.pack(padx=40, pady=(5, 40))
+    b.configure(command=lambda: entry.set(random()))
+    root.mainloop()
